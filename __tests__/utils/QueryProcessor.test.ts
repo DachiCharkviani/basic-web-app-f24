c5cf332d0d9bc8286ea61ce87552ddf67f653cbc
Dachi <user@dyn-172-20-102-101.qatar.cmu.local>
2024-10-24 06:27:51 -0700

    answered question

--- __tests__/utils/QueryProcessor.test.ts
+++ __tests__/utils/QueryProcessor.test.ts
@@ -26,4 +26,12 @@ describe("QueryProcessor", () => {
             "dcharkvi"
           ));
     });
+
+    test('should return my name', () => {
+        const query = "What is your name?";
+        const response: string = QueryProcessor(query);
+        expect(response).toBe((
+            "22694eaf"
+          ));
+    });
 });
